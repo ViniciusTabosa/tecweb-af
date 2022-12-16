3a20d812b92a9339104eec7253b7b91d6f64061e
Vinicius Tabosa <viniciustabosa052@gmail.com>
2022-12-16 09:27:19 -0300

    Create flappybird.php

---
+++ flappybird.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flappy Bird</title>
+    <link rel="stylesheet" href="css/flappy.css">
+</head>
+
+<body class="conteudo">
+    <div id="form">
+        <form action="">
+            
+        </form>
+
+    </div>
+    <h1>Flappy Bird</h1>
+    <div wm-flappy>
+
+        <!-- 
+<img class="passaro" src="img/passaro.png" alt="passaro">
+       <div class="par-de-barreiras">
+            <div class="barreira">
+                <div class="corpo"> </div>
+                <div class="borda"></div>
+            </div>
+            <div class="barreira">
+                <div class="borda"> </div>
+                <div class="corpo"></div>
+            </div>
+        </div> 
+        <div class="progresso"> 10 </div>   
+    </div>
+ -->
+
+   <!-- <script src="js/flappy.js"></script> -->
+</body>
+
+</html>
